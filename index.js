6940a922499a7ff5611692d9c414a56788868117
Kevin Lewis <kvn@lws.io>
2021-01-09 14:30:21 +0000

    Added PORT process env

--- index.js
+++ index.js
@@ -102,4 +102,4 @@ io.on('connection', (socket) => {
     })
 });
 
-http.listen(3000, console.log('Listening on port 3000'))
+http.listen(process.env.PORT || 3000, console.log('Listening on port 3000'))
